a52672b328dd28ed28dbce5956f34a2c4726899b
nthusi <pknthusi@gmail.com>
2026-01-07 12:09:58 +0300

    Add baseline migration Add refund column to billing

---
+++ migrations/20260106103440-baseline-schema.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    // No change, current databse as of migration
+  },
+
+  async down (queryInterface, Sequelize) {
+    // No change, current databse as of migration
+  }
+};
